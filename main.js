3b25afa30eac6ecc6e00ae546a901c21d6b191fd
Julia Banerjee <juliabanerjee@Julias-MacBook-Pro.local>
2021-11-15 19:42:25 +0000

    styling almost done

---
+++ main.js
@@ -0,0 +1,47 @@
+// // ideas: a) pictionary, b) hangman
+
+// a)
+// Elements:
+/* 
+- need an interactive whiteboard that you could draw on (HTML canvas?)
+- drawing tools ie rubber, colours, thickness of pencil, clear board button
+- colours and pencil thickness will need a collapsable bit with the different options. (could forgo pencil thickness if not enough time)
+- a word bank that will generate a word to draw from the word bank and not repeat itself during each game
+- displayed timer that ends the round when time is up
+- 2 different screens for player 1 and 2 (with word revealed and without)
+- point scoring if word was guessed, ie 5 if you drew it, 10 if you guessed it
+- a box showing what you've already guessed
+- advanced a thing in the top bar showing which round it is
+
+
+Advanced features:
+
+- multiplayer capability ie more than 2
+-
+
+Processes:
+
+-EXTRA a way to input player names so that player 1 name chnages to BOB etc
+- a start button that starts the rounds, triggers the wordbank and a timer of 2/3 mins
+- a board which one player could draw on and the other
+- an answerbox that displays on the other players screen or the answerbox gets greyed out depending on who's turn it is to guess (disable property?) with placeholder text
+- boolean function as to whether the word guessed matches or not if matches triggers round to stop and change turns with a congrats message
+
+
+
+
+
+Colour scheme: green main (#0A623D or #78c800#8d0lawngreenhsl(83,100%,39%)rgb(120,200,0)), orange (#FF8811), white (#FFF8F0), purple?
+
+Interactive whiteboard:
+
+<canvas>
+- you want to enable free-hand drawing mode
+- Fabric.js a js library
+- need to make is shareable real-time doc (connects users, unique for each game therefore separate page for each game) don't replicate whole board but update individual elements as the are modified
+- object added and object removed events
+
+
+
+*/
+
