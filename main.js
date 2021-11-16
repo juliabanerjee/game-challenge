b4d43b465641cc515637af818fae491bf7e109ce
Julia Banerjee <juliabanerjee@Julias-MacBook-Pro.local>
2021-11-16 16:11:38 +0000

    refining idea and testing out emojis in js

--- main.js
+++ main.js
@@ -1,33 +1,14 @@
-// // ideas: emoji game
+// create an array of onjects with emoji questions
 
-// a)
-// Elements:
-/* 
-- a screen to display emojis on
-- bank of words that go with emojis
--working timer
-- a word bank that will generate a word to draw from the word bank and not repeat itself during each game
+const emojiQuestionsArr = [
+  { question: "⬛👁️🅿️🅿️",
+    answer: "Black Eyed Peas"
+  },
+  { question: "🐑 ✌️",
+    answer: "U2"
+  },
+  { question: "🐑 🐝",
+  answer: "U2"
+}
 
-Extra:
-- advanced a thing in the top bar showing which round it is
-
-- EXTRA a box showing what you've already guessed
-
-
-Processes:
-
-
-- a start button that starts the rounds, triggers the wordbank and a timer of 2/3 mins
-
-- boolean function as to whether the word guessed matches or not if matches triggers round to stop and change turns with a congrats message
-
-
-
-
-
-Colour scheme: green main (#0A623D or #78c800#8d0lawngreenhsl(83,100%,39%)rgb(120,200,0)), orange (#FF8811), white (#FFF8F0), purple?
-
-
-
-*/
-
+]
---
+++ notes.js
@@ -0,0 +1,33 @@
+// // ideas: emoji game
+
+// a)
+// Elements:
+/* 
+- a screen to display emojis on
+- bank of words that go with emojis
+-working timer
+- a word bank that will generate a word to draw from the word bank and not repeat itself during each game
+
+Extra:
+- advanced a thing in the top bar showing which round it is
+
+- EXTRA a box showing what you've already guessed
+
+
+Processes:
+
+
+- a start button that starts the rounds, triggers the wordbank and a timer of 2/3 mins
+
+- boolean function as to whether the word guessed matches or not if matches triggers round to stop and change turns with a congrats message
+
+
+
+
+
+Colour scheme: green main (#0A623D or #78c800#8d0lawngreenhsl(83,100%,39%)rgb(120,200,0)), orange (#FF8811), white (#FFF8F0), purple?
+
+
+
+*/
+
