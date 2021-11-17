070060868da6389bde3cb3a722bc95b52ae435ef
Julia Banerjee <juliabanerjee@Julias-MacBook-Pro.local>
2021-11-17 15:05:39 +0000

    more detailed notes and starting JS

--- main.js
+++ main.js
@@ -7,8 +7,29 @@ const emojiQuestionsArr = [
   { question: "🐑 ✌️",
     answer: "U2"
   },
-  { question: "🐑 🐝",
-  answer: "U2"
+  { question: "🐑 🐝 4️⃣ 0️⃣",
+  answer: "UB40"
 }
+];
 
-]
+
+const displaySelected = document.querySelector("#emoji-display")
+
+const displayingQuestions = () => {
+  for (let i = 0; i < emojiQuestionsArr.length; i++) 
+  
+  { 
+    const formattingQuestions = `<p>${emojiQuestionsArr[i].question}</p> `
+displaySelected.innerHTML += 
+formattingQuestions;
+}
+}
+displayingQuestions()
+
+
+
+// template string
+//Setting up enter button
+const enterButton = document.querySelector("#enter")
+
+const answer = "";
--- notes.js
+++ notes.js
@@ -5,11 +5,25 @@
 /* 
 - a screen to display emojis on
 - bank of words that go with emojis
--working timer
-- a word bank that will generate a word to draw from the word bank and not repeat itself during each game
+
+- an array an of objects, loop through the array targeting the .question for each one.
+
+- enter button:
+need an eventlistener, save input in variable on handleClick
+compare input with .answer, put in function with if/else statement inside. Return a right/wrong 
+(use in loop to stop and start it)
+a) creating an input variable
+b) adding eventlistener onto neter button so that input is saved in input variable
+c) start writing function to check input against answer
+d)function needs an if/else statement and should return a right or wrong message (alert)
+e) Get that working for the first inded of the array
+f) take out of loop if it doesn't work
+
 
 Extra:
 - advanced a thing in the top bar showing which round it is
+-working timer
+-case sensitivity and accepting non-exact string inputs
 
 - EXTRA a box showing what you've already guessed
 
@@ -20,6 +34,8 @@ Processes:
 - a start button that starts the rounds, triggers the wordbank and a timer of 2/3 mins
 
 - boolean function as to whether the word guessed matches or not if matches triggers round to stop and change turns with a congrats message
+
+
 
 
 
